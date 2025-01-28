5bbcae387b0dea86c34eff907582984cce1754e9
Sara2829 <2021bec102@sggs.ac.in>
2025-01-28 12:30:32 +0530

    created a basic structure for bakend

---
+++ projectpulse/src/test/java/com/projectpulse/projectpulse/ProjectpulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectpulse.projectpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectpulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
