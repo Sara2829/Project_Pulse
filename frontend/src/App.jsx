ebb4b260cfe54c1be91c453c8ced51a3b7794f47
Sara2829 <2021bec102@sggs.ac.in>
2025-01-29 10:37:22 +0530

    created project dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Chatbot from "./components/Chatbot";
+import ProjectDashboard from "./components/ProjectDashboard";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/chatbot" element={<Chatbot />} />
+        <Route path="/projectdashboard/:projectId" element={<ProjectDashboard />} />
       </Routes>
     </Router>
   );
