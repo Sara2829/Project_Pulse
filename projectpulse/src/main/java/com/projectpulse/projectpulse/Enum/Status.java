735dd736601e1e9faaf212824f474e212b6edf46
Aditya Baheti <112819226+bahetiaditya@users.noreply.github.com>
2025-01-30 12:47:21 +0530

    Revert "added files"

--- projectpulse/src/main/java/com/projectpulse/projectpulse/Enum/Status.java
+++
@@ -1,7 +0,0 @@
-package com.projectpulse.projectpulse.Enum;
-
-public enum Status {
-    ToDo,
-    IN_PROGRESS,
-    CLOSED
-}
--- projectpulse/src/main/java/com/projectpulse/projectpulse/service/JiraService.java
+++
@@ -1,8 +0,0 @@
-package com.projectpulse.projectpulse.service;
-
-import org.springframework.http.ResponseEntity;
-
-public interface JiraService {
-    public ResponseEntity<String> getAllProjects();
-    public ResponseEntity<String> getProjectIssues(String projectId);
-}
