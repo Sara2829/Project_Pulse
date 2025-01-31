014b3f8621dcef7b8bd3ed4ac84ff3dba2a1d4c4
gayatri <patwegayatri5143@gmail.com>
2025-01-31 12:07:14 +0530

    changes in controller

--- projectpulse/src/main/java/com/projectpulse/projectpulse/service/JiraService.java
+++ projectpulse/src/main/java/com/projectpulse/projectpulse/service/JiraService.java
@@ -1,5 +1,6 @@
 package com.projectpulse.projectpulse.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import java.util.Map;
 
